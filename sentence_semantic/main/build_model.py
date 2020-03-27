99764348842b1c558dc2c8ae9f657b49017eaac1
liang.liu <920817420@qq.com>
2020-03-27 18:46:52 +0800

    ss

---
+++ sentence_semantic/main/build_model.py
@@ -0,0 +1,33 @@
+
+
+class Model():
+
+
+    #读入样本
+    def read_simples():
+        pass
+
+    #搜索&构建同义词词库
+    def build_synonyms():
+        pass
+
+    #搜索&构建句式
+    def build_patterns():
+        pass
+
+    #生成规则
+    def generate_rules():
+        pass
+
+
+    def write2file():
+        pass
+
+    #生成模型
+    def generate_stages():
+        self.read_simples()
+        self.build_synonyms()
+        self.build_patterns()
+        self.generate_rules()
+        self.write2file()
+
