1edf891a7f5ff4a3717605272b82b9ade6d2e53e
liang.liu <920817420@qq.com>
2020-03-31 19:53:48 +0800

    add

--- sentence_semantic/main/build_model.py
+++ sentence_semantic/main/build_model.py
@@ -1,33 +1,211 @@
+def get_list_product(lst_of_lst):
+    ans = [""]
+    for lst in lst_of_lst:
+        new_ans = []
+        for e in ans:
+            for e2 in lst:
+                if e2 == "": new_ans.append(e)
+                else:     new_ans.append( ("%s_%s"%(e, e2)).strip("_") )
+        ans = new_ans
+    return ans
+
+class Rule():
+
+    def __init__(self, line, words_dict):
+        self.intent = {}
+        self.rule_body = []
+        self.words_dict = words_dict
+        self.parse_rule(line)
+
+    def parse_rule_body(self, rb):
+        eles = rb.split(" ")
+        for ele in eles:
+            if "{" not in ele: continue
+                sub_eles = ele.strip("}").split("{")
+                val = sub_eles[0].strip()
+                tps = sub_eles[1].split(":")
+                sym_words = self.words_dict.find_synm(val)
+                self.rule_body.append((val, tps[0], tps[1], sym_words))
+
+    def gen_pattern(self):
+        lst_of_lst = []
+        for pat in self.rule_body:
+            if pat[3] is None:
+                lst_of_lst.append(["domian"])
+            else:
+                lst_of_lst.append(pat[3])
+
+        return set(get_list_product(lst_of_lst))
+
+    def parse_rule_intent(self, it):
+        intent = eval(it.strip())
+        for key in intent:
+            if "$" in intent[key]:
+                eles = intent[key].split("+")
+                new_eles = []
+                for e in eles:
+                    new_eles.append( int(e.strip("$")) )
+
+                self.intent[key] = new_eles
+            else:
+                self.intent[key] = intent[key]
+
+    def parse_rule(self, line):
+        eles = line.split("=>") 
+        self.parse_rule_body(eles[0]) 
+        self.parse_rule_intent(eles[1])
+
+
+def read_all_words(path, rev_dic):
+    ans = {}
+    for filename in os.listdir(path):
+        ans[filename] = []
+        new_path = os.path.join(path,filename)
+        lines = open(new_path).readlines()
+        for line in lines:
+            line =  line.strip()
+            if not line: continue
+            ans[filename].append(line)
+            if rev_dic is None: continue
+            if line not in rev_dic:
+                rev_dic[line] = []
+            rev_dic[line].append(filename)
+
+    return ans
+
+
+class WordDatabase():
+
+    def __init__(self):
+        self.word2dicname = {}
+        self.general_words = {}
+        self.domain_words = {}
+        self.general_patterns = {}
+        self.patterns2dictname = {}
+        self.build_databases()
+
+    def build_databases(self):
+        path = "../lexicon/general"
+        self.general_words = read_all_words(path, self.word2dicname)
+        self.domain_words = read_all_words(path, None)
+
+        path = "../sentence_pattern/general"
+        self.general_patterns = read_all_words(path, self.patterns2dictname)
+
+    def find_synm(self, val):
+        ans = []
+        if val in self.word2dicname:
+            for word_name in self.word2dicname[val]:
+                ans.extend(self.domain_words[word_name])
+        return ans
+
+    def find_similar_patterns(self, pat):
+        ans = []
+        if pat in self.patterns2dictname:
+            for patname in self.patterns2dictname[pat]:
+                ans.extend(self.general_patterns[patname])
+
+        return set(ans)
+        
+
+
+def gen_new_rule(pat, rules):
+    rules_size = len(rules)
+    has_seen = [ False ] * rules_size
+    pat_eles = pat.split("_")
+    new_rule = []
+    for pat in pat_eles:
+        for i in range(rules_size + 1):
+            if i >= rules_size:
+                new_rule.append(("", "", "none", [pat ]) )
+            else:
+                if has_seen[i]: continue
+                if (pat == "domian" and rules[i][1] == "_domian") or \
+                    (rules[i][3] and pat in rules[i][3] and rules[i][1] != "_domian"):
+                    new_rule.append(rules[i])
+                    has_seen[i] = True
+
+    return new_rule
 
 
 class Model():
 
+    def __init__(self, samples):
+        self.samples_file = samples
+        self.words_dict = WordDatabase()
+        self.rules = []
+        self.extend_rules = []
+
 
     #读入样本
-    def read_simples():
+    # 开通{_act:none}  手机{_domian:d1}  流量{_domian:d2} 业务{_noun:none}  =>  { "intend": "开通业务", "业务": "$2+$3" }
+    def read_simples(self):
+        lines = open("../projects/" + self.samples_file).readlines()
+        for line in lines:
+            line = line.strip()
+            if line.startswith("#"): continue
+                rule = Rule(line, self.words_dict)
+                self.rules.append(rule)
+
+
+    # 搜索&构建同义词词库
+    def build_synonyms(self, raw_rules):
         pass
 
-    #搜索&构建同义词词库
-    def build_synonyms():
-        pass
 
     #搜索&构建句式
-    def build_patterns():
-        pass
+    def build_patterns(self):
+        has_seen = set()
+        for rule in self.rules:
+            pats = rule.gen_pattern()
+            has_seen.update(pats)
+            for pat in pats:
+                extend_pats = self.words_dict.find_similar_patterns(pat)
+                for e_pat in extend_pats:
+                    if e_pat in has_seen: continue
+                    new_rule = gen_new_rule(e_pat, rule)
+                    self.extend_rules.append(new_rule)
+                    has_seen.add(e_pat)
+        
 
     #生成规则
-    def generate_rules():
-        pass
+    # export poetry7_4=[{poe操作}, {量词}?, {.古诗_作者}?, {c1}?, {.poe古诗_类别} ]
+    def generate_rules(self, rules):
+        atoms = []
+        new_rules = []
+        for rule in self.rules + self.extend_rules:
+            new_rule_parts = []
+            for ele in rule:
+                if ele[3] is None:
+                    atoms.append(ele[0])
+                    new_rule_parts.append("{}"%ele[0])
+                else:
+                    t = "|".join(["{.%s}"%ee for ee in ele[3] ])
+                    new_rule_parts.append(t)
 
+            new_rules.appen( "[" + ",".join(new_rule_parts) + "]" )
 
-    def write2file():
-        pass
+        return (atoms, new_rules)
+
+    #输出规则
+    def write2file(self, atoms, new_rules):
+        samples_lex_out_file = "../projects/" + self.samples_file + ".lex"
+        with open(samples_lex_out_file, "w") as f:
+            for atom in atoms:
+                f.write( "atom %s = %s"%(atom, atom) + "\n" )
+
+            i = 0
+            for rule in new_rules:
+                i = i + 1
+                f.write( "export rule%d = %s"%(i, rule) + "\n" )
+
+        
 
     #生成模型
-    def generate_stages():
+    def generate_stages(self):
         self.read_simples()
         self.build_synonyms()
         self.build_patterns()
-        self.generate_rules()
-        self.write2file()
+        atoms, rules = self.generate_rules()
+        self.write2file(atoms, rules)
 
---
+++ sentence_semantic/main/tokens.py
@@ -0,0 +1,182 @@
+
+from builtin import op_type, keywords, op_alp, op_info
+from syntax_check import *
+
+class token():
+    def __init__(self, tkn_type, tkn_val, line=0, col=0):
+        self.tp   = tkn_type
+        self.val  = tkn_val
+        self.line = line
+        self.col  = col
+
+    def get(self):
+        return (self.tp, self.val)
+
+    def __eq__(self, e):
+        return self.get() == e
+
+    def __repr__(self):
+        return ("type:%s; val %s; line %d col %d") %(self.tp, self.val, self.line, self.col)
+
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+class token_list():
+    def __init__(self, chars):
+        self.chars = chars
+        self.pre_token = token("SEP", "NEWLINE")
+        self.tokens = self.read_tokens()
+
+    def read_tokens(self):
+        tokens = []
+        while True:
+            self.pre_token = tkn = self.read_a_token()
+            if tkn is None:
+                return tokens
+            tokens.append(tkn)
+        
+    def read_a_token(self):
+        self.read_white_space(" \t")
+        if self.chars.eof(): return None
+        ch = self.chars.peek()
+        if ch in ('"', "'"): tkn = self.read_string()
+        elif ch == '#': tkn = self.read_note()
+        elif ch == '<': tkn = self.read_list()
+        elif ch == '[': tkn = self.read_list_f()
+        elif ch == '{': tkn = self.read_hashmap()
+        elif ch == '(': tkn = self.read_parn()
+        elif str.isdigit(ch): tkn = self.read_num()
+        elif ch in op_alp: tkn = self.read_op()
+        elif ch in ',\n;':     tkn = self.read_sep()
+        else: tkn = self.read_var()
+        return tkn
+
+    # 跳过空格
+    def read_white_space(self, ss = " \t"):
+        while not self.chars.eof() and self.chars.peek() in ss:
+            self.chars.next()
+
+    def read_var(self):
+        line, col = self.chars.line, self.chars.col
+        is_valid = lambda x: x not in '"<>{}[]()#,;\n \t' + op_alp
+        var = ""
+        while not self.chars.eof() and is_valid(self.chars.peek()):
+            var += self.chars.next()
+        if var in keywords: 
+            kw = keywords[var]
+            if kw in ["IF", "END"]:
+                return self.read_note()
+            return token(kw, var, line, col)
+        if var == "":
+            Error("Unexpected symbol", line, col)
+            
+        return token("STR", var, line, col)
+        
+    def read_list(self):
+        return self.read_pair("ANGLE", '>')
+
+    def read_list_f(self):
+        return self.read_pair("LIST", ']')
+
+    def read_hashmap(self):
+        return self.read_pair("DICT", '}')
+
+    def read_parn(self):
+        return self.read_pair("PARN",')')
+
+    # 注释
+    def read_note(self):
+        while not self.chars.eof() and self.chars.peek() != "\n":
+            self.chars.next()
+        return self.read_a_token()
+
+    # 读一个() [] or {}
+    def read_pair(self, tp, end_ch):
+        line, col = self.chars.line, self.chars.col
+        val = []
+        self.chars.next()
+        while not self.chars.eof():
+            self.read_white_space(" \t\n")
+            ch = self.chars.peek()
+            if ch == end_ch:
+                self.chars.next()
+                return token(tp, val, line, col)
+            else:
+                val.append(self.read_a_token())
+        self.chars.crack('missing ' + end_ch)
+
+    def read_sep(self):
+        line, col = self.chars.line, self.chars.col
+        ch = self.chars.next()
+        if ch == ',':
+            self.read_white_space(" \t\n")
+            return token('SEP', 'COMMA', line, col)
+        elif ch == ';':
+            self.read_white_space(" \t\n")
+            return token('SEP', 'SEMI', line, col)
+        else: 
+            return token('SEP', 'NEWLINE', line, col)
+
+    def read_op(self):
+        line, col = self.chars.line, self.chars.col
+        op = ""
+        while self.chars.peek() in op_alp:
+            op += self.chars.next()
+            if op not in op_info:
+                self.chars.back()
+                op = op[0:-1]
+                break
+        return token("OP", op_info[op], line, col)
+
+    def read_num(self):
+        line, col = self.chars.line, self.chars.col
+        ns = ""
+        has_e = False
+        while not self.chars.eof():
+            ch = self.chars.peek()
+            if str.isdigit(ch) or ch in '.Ee':
+                ns += ch
+                if ch in "Ee": has_e = True
+            elif has_e and (ch == '+' or ch == '-'):
+                ns += ch     # 科学记数法
+            else:
+                break
+
+            self.chars.next()
+            if ch not in "Ee": has_e = False
+        return token("NUM", num(ns), line, col)
+
+    # String语法与python中一样,可能是 "  ' , 多行string用""" , ''''
+    def read_string(self):
+        line, col = self.chars.line, self.chars.col
+        val = ""
+        isEscape = False
+        terminal_char = self.chars.next()
+        is_multi = self.check_multi_string(terminal_char)
+        while not self.chars.eof():
+            ch = self.chars.next()
+            if isEscape:
+                isEscape = False
+                if ch in escape_table: val += escape_table[ch]
+                else: val += "\\" + ch
+            elif ch == '\n' and not is_multi:
+                self.chars.crack('missing terminal %s'%terminal_char)
+            elif ch == '\\':
+                isEscape = True
+            elif (ch == terminal_char) and (not is_multi \
+                     or self.check_multi_string(ch)) :
+                return token('STRING', val, line, col)
+            else:
+                val += ch
+
+        self.chars.crack('missing terminal %s'%terminal_char)
+
+    def check_multi_string(self, tc):
+        if tc != self.chars.peek():  return False
+        if self.chars.eof() or tc != self.chars.looknext(): return False
+        self.chars.next(), self.chars.next()
+        return True
